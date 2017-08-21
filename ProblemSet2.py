ac858e7ecbfad6dd87b8e920f28d4da87013ceaa
Tong-Kiat Tan <tktan@users.noreply.github.com>
2017-08-21 11:24:43 +0800

    Add problem2_8 doctests

--- ProblemSet2.py
+++ ProblemSet2.py
@@ -432,6 +432,23 @@ hourly_temp = [40.0, 39.0, 37.0, 34.0, 33.0, 34.0, 36.0, 37.0, 38.0, 39.0, \
                36.0, 35.0, 33.0, 32.0]
 #%%
 def problem2_8(temp_list):
+    """
+    >>> hourly_temp = [40.0, 39.0, 37.0, 34.0, 33.0, 34.0, 36.0, 37.0, 38.0, 39.0, \
+                       40.0, 41.0, 44.0, 45.0, 47.0, 48.0, 45.0, 42.0, 39.0, 37.0, \
+                       36.0, 35.0, 33.0, 32.0]
+    >>> problem2_8(hourly_temp)    # doctest: +SKIP
+    Average: 38.791666666666664
+    High: 48.0
+    Low: 32.0
+
+    >>> import random
+    >>> random.seed(439388)
+    >>> problem2_8([float(random.randrange(100)) for i in range(42)])    # doctest: +SKIP
+    Average: 51.42857142857143
+    High: 99.0
+    Low: 1.0
+    """
+
     pass # replace this pass (a do-nothing) statement with your code
 
 
